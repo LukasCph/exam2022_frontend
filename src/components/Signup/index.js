f47c2cc280d7cf87994bef5edc1bbe02f5d156f4
LukasCph <cph-lo140@cphbusiness.dk>
2022-01-12 19:58:20 +0100

    initial commit adding files

---
+++ src/components/Signup/index.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+// Facade
+import { facade } from '../../apiFacade';
+import { signup } from '../../settings';
+
+// Styles
+import {
+  Wrapper,
+  StyledFormWrapper,
+  StyledForm,
+  StyledInput,
+  StyledButton,
+  StyledError,
+} from './Signup.styles';
+
+//Router
+import { useNavigate } from 'react-router-dom';
+
+function Signup({ setLoginCredentials, setLoggedIn }) {
+  const [response, setResponse] = useState('');
+  const [signupCredentials, setSignupCredentials] = useState({});
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setSignupCredentials({
+      ...signupCredentials,
+      [e.target.id]: e.target.value,
+    });
+    // console.log(signupCredentials);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await facade.signup(signupCredentials.username, signupCredentials.password);
+      if (response.status === 'success') {
+        facade
+          .login(signupCredentials.username, signupCredentials.password)
+          .then(() => {
+            if (sessionStorage.getItem('username') && facade.loggedIn) {
+              setLoginCredentials(signupCredentials);
+              setLoggedIn(true);
+              navigate('/');
+            }
+          });
+      }
+  };
+
+  return (
+    <Wrapper>
+      <StyledFormWrapper>
+        <StyledForm>
+          <StyledInput
+            placeholder="User Name"
+            id="username"
+            onChange={handleChange}
+          />
+          <StyledInput
+            placeholder="Password"
+            type="password"
+            id="password"
+            onChange={handleChange}
+          />
+          {response.message && (
+            <StyledError>
+              <p>{response.message}</p>
+            </StyledError>
+          )}
+          <StyledButton onClick={handleSubmit}>Sign up</StyledButton>
+        </StyledForm>
+      </StyledFormWrapper>
+    </Wrapper>
+  );
+}
+
+export default Signup;
---
+++ src/settings.js
@@ -0,0 +1,8 @@
+//Base URL
+
+const urlDroplet = "https://arolander.com/tomcat/exam";
+const urlLocal = "http://localhost:8080/exam";
+export const myUrl = urlLocal;
+
+export const login = `${myUrl}/api/login`;
+export const signup = `${myUrl}/api/login/create`;
